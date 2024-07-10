03ded41bf2fed0cac64b595afc62f4ba580caf21
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-07-10 22:29:45 +0300

    Added Raylib as a dependency to the project.

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,19 @@
-#include <fmt/core.h>
+#include "raylib.h"
 
 int main() {
-    fmt::print("Hello, {}!\n", "world");
+    // Initialize the window
+    InitWindow(800, 600, "Raylib Test");
+
+    // Main game loop (runs until the window should close)
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawText("Raylib is working!", 190, 200, 20, RED);
+        EndDrawing();
+    }
+
+    // De-initialize the window
+    CloseWindow();
+
     return 0;
 }
